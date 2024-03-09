6d51394aebc300f1ff98d4f0d7c2b6e074516b2c
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-09 15:52:39 +0200

    feat: added madimi_one local font and setup app directory structure

---
+++ src/app/components/general/fonts.ts
@@ -0,0 +1,11 @@
+// import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
+
+// export const poppins = Poppins({
+//    subsets: ["latin"],
+//    display: "swap",
+//    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+//    variable: "--font-poppins",
+// });
+
+export const madimiOne = localFont({ src: "../../../../public/fonts/MadimiOne-Regular.ttf" });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-   return <main> Hello NextJs</main>;
+   return (
+      <main className="text-center p-7">
+         <h1>Hello NextJs</h1>
+         <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident omnis consectetur enim veritatis ut magni
+            esse deserunt itaque expedita minus excepturi iure odit aperiam, quo similique, dolore architecto reprehenderit
+            id adipisci. Illo placeat unde et recusandae voluptates minima quos dolore eligendi tempore reiciendis,
+            necessitatibus, iure, perspiciatis sit laborum sint a?
+         </p>
+      </main>
+   );
 }

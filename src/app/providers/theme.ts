5c0b9e8d6df69b1ece0f8ef9fb4140f6b89babe9
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-09 17:02:03 +0200

    fix: made theme a client component

--- src/app/providers/theme.ts
+++ src/app/providers/theme.ts
@@ -1,45 +1,38 @@
-import { createTheme } from "@mui/material";
+"use client";
 
-const theme = createTheme({
-   // palette: {
-   //    text: {
-   //       primary: "rgb(80,80,80)",
-   //       secondary: "rgb(120,120,120)",
-   //    },
-   //    background: {
-   //       default: "whitesmoke",
-   //    },
-   //    primary: {
-   //       light: "rgb(27, 68, 104)",
-   //       main: "rgb(17, 46, 72)",
-   //       dark: "rgb(3, 17, 29)",
-   //    },
-   //    secondary: {
-   //       light: "rgb(115, 115, 115)",
-   //       main: "rgb(89, 89, 89)",
-   //       dark: "rgb(64, 64, 64)",
-   //    },
-   // },
-   // components: {
-   //    MuiButton: {
-   //       defaultProps: {
-   //          variant: "contained",
-   //          size: "small",
-   //          sx: { px: 2 },
-   //       },
-   //       styleOverrides: {
-   //          root: {
-   //             borderRadius: "20px",
-   //             textTransform: "capitalize",
-   //          },
-   //       },
-   //    },
-   //    MuiTextField: {
-   //       defaultProps: {
-   //          sx: { "& .MuiInputBase-root": { borderRadius: "20px" } },
-   //       },
-   //    },
-   // },
-});
+import { ThemeOptions, createTheme } from "@mui/material";
+
+export const themeOptions: ThemeOptions = {
+   palette: {
+      primary: {
+         main: "#b58536",
+      },
+      secondary: {
+         main: "rgba(93,118,138,0.69)",
+      },
+   },
+   components: {
+      MuiButton: {
+         defaultProps: {
+            variant: "contained",
+            size: "small",
+            sx: { px: 2 },
+         },
+         styleOverrides: {
+            root: {
+               borderRadius: "20px",
+               textTransform: "capitalize",
+            },
+         },
+      },
+      MuiTextField: {
+         defaultProps: {
+            sx: { "& .MuiInputBase-root": { borderRadius: "20px" } },
+         },
+      },
+   },
+};
+
+const theme = createTheme(themeOptions);
 
 export default theme;

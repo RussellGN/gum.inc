a2bd5d45e2e9b9440d2fbfb3b317bc9a8868606a
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-09 17:02:45 +0200

    move homepage to normal-pages route group

---
+++ src/app/(normal-pages)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export default function Home() {
+   return (
+      <main className="text-center p-7">
+         <h1>GUM INC</h1>
+      </main>
+   );
+}

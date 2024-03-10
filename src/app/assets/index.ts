6354a7697b95dc5806183af0c9d541eea25896bd
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-10 22:36:32 +0200

    chore: imported images

---
+++ src/app/assets/index.ts
@@ -0,0 +1,41 @@
+import artShop from "./images/artShop.jpg";
+import artShop2 from "./images/artShop2.jpg";
+import firm from "./images/firm.jpg";
+import firm2 from "./images/firm2.jpg";
+import user1 from "./images/user.jpg";
+import user2 from "./images/user2.jpg";
+import user3 from "./images/user3.jpg";
+import user4 from "./images/user4.jpg";
+import user5 from "./images/user5.jpg";
+import ukFlag from "./svgs/ukFlag.svg";
+import worldFlag from "./svgs/worldFlag.svg";
+import blob1 from "./svgs/blob1.svg";
+import blob2 from "./svgs/blob2.svg";
+import blob3 from "./svgs/blob3.svg";
+import undraw_connected from "./svgs/undraw_connected.svg";
+import undraw_connected_2 from "./svgs/undraw_connected_2.svg";
+import undraw_online_resume from "./svgs/undraw_online_resume.svg";
+import undraw_predictive_analytics from "./svgs/undraw_predictive_analytics.svg";
+
+const images = {
+   artShop,
+   artShop2,
+   firm,
+   firm2,
+   user1,
+   user2,
+   user3,
+   user4,
+   user5,
+   ukFlag,
+   worldFlag,
+   blob1,
+   blob2,
+   blob3,
+   undraw_connected,
+   undraw_connected_2,
+   undraw_online_resume,
+   undraw_predictive_analytics,
+};
+
+export default images;

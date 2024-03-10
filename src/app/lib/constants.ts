d55fe04f229a8f39f285efedead7c7282da8c872
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-10 22:50:41 +0200

    feat: setup initial hooks, lib, types and interfaces

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,28 @@
+import { link } from "../types";
+
+export const navLinks: link[] = [
+   {
+      label: "Home",
+      path: "/",
+   },
+   {
+      label: "Connect",
+      path: "/connect",
+   },
+   {
+      label: "Inc Office",
+      path: "/inc-office",
+   },
+   {
+      label: "About",
+      path: "/about",
+   },
+];
+
+export const zIndices = {
+   header: 99,
+   mobileNavigation: 100,
+};
+
+export const primaryColor = "#b58536";
+export const secondaryColor = "#365da1";
---
+++ src/app/lib/utils.ts
@@ -0,0 +1,58 @@
+import { redirect } from "next/navigation";
+
+export function generateAvatarLetters(string: string): string {
+   string = string.trim();
+   const words = string.split(" ");
+   const letters = words.map((word) => word[0]);
+
+   const finalLetters = letters.join("").toUpperCase();
+
+   if (finalLetters.length > 1) return finalLetters.slice(0, 2);
+   else return finalLetters;
+}
+
+export async function wait(seconds: number, log?: boolean) {
+   if (log) console.log("waiting");
+   await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+   if (log) console.log("done waiting");
+}
+
+export function setCookie(cname: string, cvalue: string, exdays: number) {
+   try {
+      const d = new Date();
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+      const expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+   } catch (error) {
+      console.log("error getting cookie:", error);
+   }
+}
+
+export function deleteCookie(cname: string) {
+   try {
+      document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+   } catch (error) {
+      console.log("error deleting cookie:", error);
+   }
+}
+
+export function getCookie(cname: string) {
+   try {
+      const name = cname + "=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(";");
+      for (let i = 0; i < ca.length; i++) {
+         let c = ca[i];
+         while (c.charAt(0) == " ") {
+            c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+         }
+      }
+      return null;
+   } catch (error) {
+      console.log("error getting cookie:", error);
+      return null;
+   }
+}

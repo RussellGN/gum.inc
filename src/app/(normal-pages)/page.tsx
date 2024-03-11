2aec01f8f2303dc55a8856de264decfa90e4a0cc
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-11 21:16:39 +0200

    feat: create search cta

--- src/app/(normal-pages)/page.tsx
+++ src/app/(normal-pages)/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
    return (
       <>
          <Hero />
-         <br />
-         <br />
          <SearchCta />
          <br />
          <br />
--- src/app/components/home/SearchCta.tsx
+++ src/app/components/home/SearchCta.tsx
@@ -1,5 +1,88 @@
+import images from "@/app/assets";
+import { secondaryColor } from "@/app/lib/constants";
+import { Search } from "@mui/icons-material";
+import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 export default function SearchCta() {
-   return <>SearchCta</>;
+   return (
+      <div className="min-h-[70vh] flex items-center">
+         <Box
+            className="py-20 w-full  flex flex-col items-center justify-center gap-3 relative"
+            // sx={{
+            //    backgroundImage: `url(${(images.layered_waves_haikei_4 as StaticImageData).src})`,
+            //    backgroundRepeat: "no-repeat",
+            //    backgroundPosition: "center",
+            //    backgroundSize: "cover",
+            // }}
+         >
+            <Image
+               src={images.blob1}
+               alt=""
+               className="w-full absolute  top-1/2 -left-20 -z-10"
+               style={{ transform: "translateY(-50%" }}
+            />
+            <Box
+               className="shadow-xl"
+               sx={{
+                  position: "absolute",
+                  top: "-1rem",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  background: "white",
+                  border: "solid 2px",
+                  borderColor: "divider",
+                  borderRadius: "20px",
+                  px: 4,
+                  py: 1,
+               }}
+            >
+               <Typography variant="h2">Already know what you&apos;re looking for?</Typography>
+            </Box>
+            {/* <Typography variant="h2">Lorem ipsum dolor sit amet</Typography> */}
+            <Typography className="max-w-prose text-center">
+               Lorem ipsum, dolor sit amet consectetur <br />
+               adipisicing elit. Vitae, labore fugit iste, quo nostrum, aut minima animi id <br /> accusamus quibusdam
+            </Typography>
+
+            {/* <div className="w-1/2 mt-5">
+               <TextField
+                  color="secondary"
+                  variant="filled"
+                  fullWidth
+                  label="Search"
+                  placeholder="people, businesses and others..."
+                  sx={{ "& .MuiInputBase-root": { borderRadius: "20px 20px 0 0" } }}
+               />
+            </div> */}
+            <div className="w-1/2 mt-5 flex">
+               <input
+                  className="flex-grow"
+                  color="secondary"
+                  name="q"
+                  type="search"
+                  placeholder="Search people, businesses and others..."
+                  style={{
+                     borderRadius: "50px 0 0 50px ",
+                     border: "solid thin",
+                     borderColor: secondaryColor,
+                     boxShadow: "none",
+                     textAlign: "center",
+                     padding: "1rem 2rem ",
+                  }}
+               />
+               <Button
+                  sx={{
+                     border: "solid thin",
+                     borderColor: secondaryColor,
+                     borderRadius: "0 50px 50px 0",
+                  }}
+               >
+                  <Search />
+               </Button>
+            </div>
+         </Box>
+      </div>
+   );
 }

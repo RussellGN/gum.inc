f798161646d66194ae5fda93ee90e6785a62bbec
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-18 14:54:53 +0200

    feat: create inc connect sidebar

---
+++ src/app/components/inc-connect/Sidebar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { directories } from "@/app/lib/constants";
+import { Fullscreen, FullscreenExit, Maximize, Minimize, Search, Tune } from "@mui/icons-material";
+import { Box, Button, Grid, IconButton, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import DirectoryCard from "./DirectoryCard";
+import { expandSize } from "@/app/types";
+import Image from "next/image";
+import images from "@/app/assets";
+
+export default function Sidebar() {
+   const [expand, setExpand] = useState<expandSize>("normal");
+
+   return (
+      <Grid item xs md={expand === "normal" ? 2.5 : "auto"}>
+         <Box className="h-[80vh] overflow-y-auto border rounded-[15px] bg-white p-4 sticky top-[100px]">
+            {/* {expand === "minimized" ? (
+               <Button endIcon={<Fullscreen />}>Maximize</Button>
+            ) : (
+               <Button endIcon={<FullscreenExit />}>Minimize </Button>
+            )} */}
+
+            <Image
+               src={images.undraw_location_search_re_ttoj}
+               alt="online search"
+               className="w-full mb-4 mx-auto max-w-[7rem]"
+            />
+
+            <Typography variant="h5" textAlign="center">
+               Almanacs & Directories
+            </Typography>
+
+            <div className="flex justify-center gap-2 items-center mt-3 mb-5">
+               <TextField label="Search..." placeholder="e.g Finance" size="small" />
+            </div>
+
+            <ul className="list-none">
+               {directories.map((dir) => (
+                  <li key={dir.name}>
+                     <DirectoryCard dir={dir} expand={expand} />
+                  </li>
+               ))}
+            </ul>
+         </Box>
+      </Grid>
+   );
+}

f19dcdae14a4091ebe80231ca6b13beec516c8d2
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-18 14:47:51 +0200

    feat: added directory and expandSize types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -2,3 +2,11 @@ export type link = {
    label: string;
    path: string;
 };
+
+export type directory = {
+   name: string;
+   about: string;
+   icon: JSX.Element;
+};
+
+export type expandSize = "minimized" | "normal" | "expanded";

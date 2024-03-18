882f8ff4161b4d3ec0429c9ede9cb642e61b65e9
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-18 14:50:48 +0200

    feat: create userInterface

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,1 +1,24 @@
+import { StaticImageData } from "next/image";
 
+export interface UserInterface {
+   id: number;
+   slug: string;
+   name: string;
+   email: string;
+   jobTitle: string;
+   industry: string;
+   location: string;
+   verified: boolean;
+   bio: string;
+   displayPhoto: string | StaticImageData;
+   serviceOfferings: {
+      service: string;
+      description: string;
+   }[];
+   images: (string | StaticImageData)[];
+   socialMedia: {
+      platform: string;
+      username: string;
+      url: string;
+   }[];
+}

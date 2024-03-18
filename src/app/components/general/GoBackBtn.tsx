4fae68221c165b94b2bb4ceb45a25502ae377f82
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-18 15:01:26 +0200

    feat: create go back button

---
+++ src/app/components/general/GoBackBtn.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ArrowBack } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+
+export default function GoBackBtn() {
+   const router = useRouter();
+
+   return (
+      <Button
+         onClick={() => router.back()}
+         size="large"
+         sx={{ border: "solid thin", borderColor: "divider", px: 3, mb: 3 }}
+         startIcon={<ArrowBack />}
+      >
+         Go Back
+      </Button>
+   );
+}

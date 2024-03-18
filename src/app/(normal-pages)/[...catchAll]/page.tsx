d1186e8dd36d0c5bcd0af25c8b07764126579f7e
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-18 14:46:56 +0200

    refactor: updated catch-all page structure

--- src/app/(normal-pages)/[...catchAll]/page.tsx
+++ src/app/(normal-pages)/[...catchAll]/page.tsx
@@ -1,3 +1,17 @@
+import { Home } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import Link from "next/link";
+
 export default function Page() {
-   return <h2>Page not found</h2>;
+   return (
+      <div className="min-h-[70vh] flex items-center justify-center">
+         <Typography textAlign="center">
+            This page is either still in development or does not exist
+            <br />
+            <Button startIcon={<Home />} component={Link} href="/" sx={{ mt: 2 }}>
+               Homepage
+            </Button>
+         </Typography>
+      </div>
+   );
 }

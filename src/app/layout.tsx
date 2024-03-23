548349373ec6730cf786f0dd4c661c4222b79f44
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-24 00:03:12 +0200

    feat: create desktop only barrier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Providers from "./providers/Providers";
 import { madimiOne } from "./components/general/fonts";
+import { Typography } from "@mui/material";
 
 export const metadata: Metadata = {
    title: "Gum.iNc",
@@ -12,7 +13,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
    return (
       <Providers>
          <html lang="en">
-            <body className={madimiOne.className}>{children}</body>
+            <body className={madimiOne.className}>
+               <div className="xl:hidden">
+                  <Typography className="p-3 h-[100vh] flex items-center justify-center text-center">
+                     Currently not viewable on mobile
+                  </Typography>
+               </div>
+
+               <div className="hidden lg:block">{children} </div>
+            </body>
          </html>
       </Providers>
    );

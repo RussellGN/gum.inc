8f6c349a0200c418b2ab8f035d8180c4c5462d37
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-24 00:07:13 +0200

    refactor: refactor join card elements

--- src/app/components/inc-connect/JoinCard.tsx
+++ src/app/components/inc-connect/JoinCard.tsx
@@ -1,16 +1,19 @@
-import { primaryColor, secondaryColor } from "@/app/lib/constants";
+import { gradients, primaryColor, secondaryColor } from "@/app/lib/constants";
 import { AddCircleOutlined } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function JoinCard({ directory }: { directory: string }) {
    return (
       <Box
-         className="border-[10px] border-double border-white rounded-[15px]"
-         sx={{ background: `linear-gradient(to bottom right, ${primaryColor}, goldenrod)` }}
+         component={Link}
+         href="/join"
+         className="border-[10px] border-double border-[transparent] rounded-[15px] transition-all  hover:border-white"
+         sx={{ background: gradients.primary1 }}
       >
          <Box className="p-3 h-full flex flex-col text-white items-center justify-center gap-3">
             <AddCircleOutlined fontSize="large" />
-            <Typography fontWeight="bold" noWrap>
+            <Typography variant="h2" textAlign="center">
                Join {directory}
             </Typography>
          </Box>

5569233a3a4f2fe46e8362683f6f47569f4aa165
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-24 22:46:53 +0200

    feat: add new interfaces

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,4 +1,5 @@
 import { StaticImageData } from "next/image";
+import { notificationType } from "../types";
 
 export interface UserInterface {
    id: number;
@@ -21,4 +22,26 @@ export interface UserInterface {
    }[];
    displayPhoto: string | StaticImageData;
    images: (string | StaticImageData)[];
+}
+
+export interface NotificationInterface {
+   id: number;
+   title: string;
+   user: UserInterface;
+   type: notificationType;
+   content: string;
+   date: string;
+   read: boolean;
+}
+
+export interface SubscriptionInterface {
+   id: number;
+   user: UserInterface;
+   name: string;
+   price: string;
+   renews: boolean;
+   date: string;
+   expires?: string;
+   description: string;
+   icon: JSX.Element;
 }

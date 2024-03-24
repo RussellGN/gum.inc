7af4eb38492d0b33def674ed10dcd3b81a849994
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-24 22:53:28 +0200

    fix, doc: add keys for array-jsx mappings, reomve unused imports and remove commented code

--- src/app/components/home/DirectoriesTabs.tsx
+++ src/app/components/home/DirectoriesTabs.tsx
@@ -1,22 +1,8 @@
 "use client";
 
-import images from "@/app/assets";
 import { sampleUsers } from "@/app/lib/sampleData";
-import {
-   Event,
-   HolidayVillage,
-   LocationOn,
-   MedicalServices,
-   Medication,
-   Message,
-   Money,
-   MusicNote,
-   Verified,
-   Work,
-} from "@mui/icons-material";
-import { Typography, Box, Tab, Tabs, SvgIconTypeMap, IconButton, Button } from "@mui/material";
-import { OverridableComponent } from "@mui/material/OverridableComponent";
-import Image from "next/image";
+import { Event, MedicalServices, Money, MusicNote } from "@mui/icons-material";
+import { Box, Tab, Tabs } from "@mui/material";
 import React, { ReactNode } from "react";
 import UserCard from "../inc-connect/UserCard";
 
@@ -39,6 +25,7 @@ export default function DirectoriesTabs() {
             >
                {tabs.map((tab, index) => (
                   <Tab
+                     key={tab.label}
                      sx={{ textTransform: "capitalize" }}
                      color="secondary"
                      iconPosition="start"
@@ -52,7 +39,7 @@ export default function DirectoriesTabs() {
 
          <Box>
             {tabs.map((tab, index) => (
-               <CustomTabPanel value={value} index={index}>
+               <CustomTabPanel key={"panel-" + tab.label} value={value} index={index}>
                   {tab.content}
                </CustomTabPanel>
             ))}
@@ -112,47 +99,6 @@ function CustomTabPanel(props: TabPanelProps) {
          {...other}
       >
          {value === index && (
-            // <Box sx={{ p: 3 }} className="flex overflow-x-auto gap-5">
-            //    {[1, 2, 3, 4, 5].map((num) => (
-            //       <div className="bg-[whitesmoke] border rounded-md shadow-md ">
-            //          <div className="p-1">
-            //             <div className="flex gap-3 items-center px-2">
-            //                <Image
-            //                   src={images.user1}
-            //                   alt="user"
-            //                   className="w-9 h-9 object-cover rounded-[100%] border shadow-md"
-            //                />
-            //                <div className="mr-auto ml-2">
-            //                   <Typography variant="caption">Will Cosby</Typography>
-            //                </div>
-            //                <Verified fontSize="small" />
-            //             </div>
-
-            //             <div className="flex gap-3 flex-wrap pt-4 px-4">
-            //                <Box sx={{ bgcolor: "divider", width: "10%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "40%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "60%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "30%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "80%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "10%", height: "0.4rem", m: 0.05 }}></Box>
-            //                <Box sx={{ bgcolor: "divider", width: "50%", height: "0.4rem", m: 0.05 }}></Box>
-            //             </div>
-
-            //             <div className="flex gap-2 justify-around mt-4 px-2">
-            //                <Typography
-            //                   variant="subtitle1"
-            //                   className=" flex items-center gap-2 p-1 justify-center text-center"
-            //                >
-            //                   <LocationOn /> Ipswich
-            //                </Typography>
-            //                <IconButton color="secondary" size="small">
-            //                   <Message />
-            //                </IconButton>
-            //             </div>
-            //          </div>
-            //       </div>
-            //    ))}{" "}
-            // </Box>
             <Box sx={{ p: 2 }} className="grid grid-flow-col auto-cols-auto overflow-x-auto gap-3">
                {sampleUsers.map((user) => (
                   <div className="w-[10rem]">

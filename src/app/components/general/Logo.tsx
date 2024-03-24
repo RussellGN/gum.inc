f08e1f9d90a187caabee4cf533cce27d3354c77a
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-24 22:50:34 +0200

    refactor: make logo bigger

--- src/app/components/general/Logo.tsx
+++ src/app/components/general/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Logo() {
    return (
-      <Typography component={Link} href="/" color="primary" variant="body1">
+      <Typography component={Link} href="/" color="primary" variant="h3">
          GUM.INC
       </Typography>
    );

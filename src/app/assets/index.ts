fdfef6abf0584af3a3612f2fb5b182e863ba3d83
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-28 11:59:03 +0200

    assets: add more diverse assets

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,32 +1,22 @@
-import logo1 from "./logos/logo1.jpg";
-import logo2 from "./logos/logo2.jpg";
-import logo3 from "./logos/logo3.jpg";
+import guminc_logo1 from "./logos/logo1.jpg";
+import guminc_logo2 from "./logos/logo2.jpg";
+import guminc_logo3 from "./logos/logo3.jpg";
 import connectScreenshot from "./images/connectScreenshot.png";
+
 import artShop from "./images/artShop.jpg";
 import artShop2 from "./images/artShop2.jpg";
 import firm from "./images/firm.jpg";
 import firm2 from "./images/firm2.jpg";
-import user1 from "./images/user.jpg";
-import user2 from "./images/user2.jpg";
-import user3 from "./images/user3.jpg";
-import user4 from "./images/user4.jpg";
-import user5 from "./images/user5.jpg";
-import ukFlag from "./svgs/ukFlag.svg";
 
-import worldFlag from "./svgs/worldFlag.svg";
+import userm from "./images/userm.jpg";
+import user2m from "./images/user2m.jpg";
+import user3f from "./images/user3f.jpg";
+import user4f from "./images/user4f.jpg";
+import user5m from "./images/user5m.jpg";
 
 import blob1 from "./svgs/blob1.svg";
 import blob2 from "./svgs/blob2.svg";
 import blob3 from "./svgs/blob3.svg";
-import undraw_connected from "./svgs/undraw_connected.svg";
-import undraw_connected_2 from "./svgs/undraw_connected_2.svg";
-import undraw_online_resume from "./svgs/undraw_online_resume.svg";
-import undraw_predictive_analytics from "./svgs/undraw_predictive_analytics.svg";
-import undraw_newsletter_re_wrob from "./svgs/undraw_newsletter_re_wrob.svg";
-import undraw_mailbox_re_dvds from "./svgs/undraw_mailbox_re_dvds.svg";
-import undraw_online_ad_re_ol62 from "./svgs/undraw_online_ad_re_ol62.svg";
-import undraw_the_search_s0xf from "./svgs/undraw_the_search_s0xf.svg";
-
 import layered_steps_haikei from "./svgs/layered-steps-haikei.svg";
 import layered_steps_haikei_2 from "./svgs/layered-steps-haikei2.svg";
 import layered_steps_haikei_4 from "./svgs/layered-steps-haikei4.svg";
@@ -38,6 +28,16 @@ import stacked_steps_haikei from "./svgs/stacked-steps-haikei.svg";
 import stacked_steps_haikei_2 from "./svgs/stacked-steps-haikei2.svg";
 import stacked_waves_haikei from "./svgs/stacked-waves-haikei.svg";
 
+import ukFlag from "./svgs/ukFlag.svg";
+import worldFlag from "./svgs/worldFlag.svg";
+import undraw_connected from "./svgs/undraw_connected.svg";
+import undraw_connected_2 from "./svgs/undraw_connected_2.svg";
+import undraw_online_resume from "./svgs/undraw_online_resume.svg";
+import undraw_predictive_analytics from "./svgs/undraw_predictive_analytics.svg";
+import undraw_newsletter_re_wrob from "./svgs/undraw_newsletter_re_wrob.svg";
+import undraw_mailbox_re_dvds from "./svgs/undraw_mailbox_re_dvds.svg";
+import undraw_online_ad_re_ol62 from "./svgs/undraw_online_ad_re_ol62.svg";
+import undraw_the_search_s0xf from "./svgs/undraw_the_search_s0xf.svg";
 import undraw_web_search_re_efla from "./svgs/undraw_web_search_re_efla.svg";
 import undraw_adventure_re_ncqp from "./svgs/undraw_adventure_re_ncqp.svg";
 import undraw_file_searching_re_3evy from "./svgs/undraw_file_searching_re_3evy.svg";
@@ -45,12 +45,50 @@ import undraw_location_search_re_ttoj from "./svgs/undraw_location_search_re_tto
 import undraw_people_search_re_5rre from "./svgs/undraw_people_search_re_5rre.svg";
 import undraw_search_app_oso2 from "./svgs/undraw_search_app_oso2.svg";
 import undraw_team_re_0bfe from "./svgs/undraw_team_re_0bfe.svg";
-
 import undraw_newspaper_re_syf5 from "./svgs/undraw_newspaper_re_syf5.svg";
 import undraw_profile_data_re_v81r from "./svgs/undraw_profile_data_re_v81r.svg";
 import undraw_segment_analysis_re_ocsl from "./svgs/undraw_segment_analysis_re_ocsl.svg";
 import undraw_social_strategy_re_mk87 from "./svgs/undraw_social_strategy_re_mk87.svg";
 import undraw_stepping_up_g6oo from "./svgs/undraw_stepping_up_g6oo.svg";
+
+import auto_shop_2 from "./images/auto_shop_2.jpg";
+import auto_shop_3 from "./images/auto_shop_3.png";
+import auto_shop_5 from "./images/auto_shop_5.jpeg";
+import auto_shop_logo from "./images/auto_shop_logo.jpg";
+import auto_shop_logo2 from "./images/auto_shop_logo2.webp";
+import event from "./images/event.webp";
+import event2 from "./images/event2.jpeg";
+import event3 from "./images/event3.jpg";
+import event5 from "./images/event5.jpg";
+import event6 from "./images/event6.jpg";
+import event7 from "./images/event7.png";
+import farm from "./images/farm.jpg";
+import farm1 from "./images/farm1.jpg";
+import farm_logo from "./images/farm_logo.jpg";
+import farm_logo2 from "./images/farm_logo2.png";
+import furniture from "./images/furniture.jpg";
+import garden from "./images/garden.webp";
+import logo from "./images/logo.png";
+import logo2 from "./images/logo2.jpg";
+import logo3 from "./images/logo3.jpg";
+import office from "./images/office.jpg";
+import office1 from "./images/office1.jpg";
+import office2 from "./images/office2.jpg";
+import person1m from "./images/person1m.jpg";
+import person2m from "./images/person2m.jpg";
+import person3f from "./images/person3f.webp";
+import person4m from "./images/person4m.jpg";
+import person5f from "./images/person5f.jpg";
+import person6f from "./images/person6f.jpg";
+import person8m from "./images/person8m.webp";
+import person9m from "./images/person9m.jpg";
+import study_abroad from "./images/study_abroad.jpg";
+import restaurant from "./images/restaurant.jpg";
+import restaurant2 from "./images/restaurant2.jpg";
+import restaurant3 from "./images/restaurant3.jpg";
+import types from "./images/types-uk-company.jpg";
+import vinatge_cars from "./images/vinatge_cars.jpg";
+import visa_consultancy from "./images/visa_consultancy.png";
 
 // import { StaticImageData } from "next/image";
 
@@ -108,19 +146,19 @@ import undraw_stepping_up_g6oo from "./svgs/undraw_stepping_up_g6oo.svg";
 // export default images;
 
 export default {
-   logo1,
-   logo2,
-   logo3,
+   guminc_logo1,
+   guminc_logo2,
+   guminc_logo3,
    connectScreenshot,
    artShop,
    artShop2,
    firm,
    firm2,
-   user1,
-   user2,
-   user3,
-   user4,
-   user5,
+   userm,
+   user2m,
+   user3f,
+   user4f,
+   user5m,
    ukFlag,
    worldFlag,
    blob1,
@@ -156,4 +194,42 @@ export default {
    undraw_segment_analysis_re_ocsl,
    undraw_social_strategy_re_mk87,
    undraw_stepping_up_g6oo,
+   auto_shop_2,
+   auto_shop_3,
+   auto_shop_5,
+   auto_shop_logo,
+   auto_shop_logo2,
+   event,
+   event2,
+   event3,
+   event5,
+   event6,
+   event7,
+   farm,
+   farm1,
+   farm_logo,
+   farm_logo2,
+   furniture,
+   garden,
+   logo,
+   logo2,
+   logo3,
+   office,
+   office1,
+   office2,
+   person1m,
+   person2m,
+   person3f,
+   person4m,
+   person5f,
+   person6f,
+   person8m,
+   person9m,
+   study_abroad,
+   restaurant,
+   restaurant2,
+   restaurant3,
+   types,
+   vinatge_cars,
+   visa_consultancy,
 };

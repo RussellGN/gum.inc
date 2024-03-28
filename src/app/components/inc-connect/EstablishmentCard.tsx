ed3ad0f164f328127c1a14355b24bf20afe4aaab
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-03-28 12:09:34 +0200

    feat: create ui cards for new listing classes 'establishment' and 'event'

---
+++ src/app/components/inc-connect/EstablishmentCard.tsx
@@ -0,0 +1,63 @@
+import { EstablishmentInterface } from "@/app/interfaces";
+import { Business, LocationOn, Verified } from "@mui/icons-material";
+import { Box, Grid, Typography } from "@mui/material";
+import Link from "next/link";
+import AspectContainedNextImage from "../general/AspectContainedNextImage";
+import SaveIncButton from "./SaveIncButton";
+
+export default function EstablishmentCard({
+   establishment,
+   simplifified,
+}: {
+   establishment: EstablishmentInterface;
+   simplifified?: boolean;
+}) {
+   if (simplifified) {
+      console.log("simplified");
+   }
+
+   return (
+      <Box className="block border rounded-[15px] bg-slate-200 ">
+         <Box
+            component={Link}
+            href={`/inc-connect/establishment/${establishment.slug}`}
+            className="block border pb-2 border-b-0 border-[transparent] rounded-tl-[15px] rounded-tr-[15px] cursor-pointer transition-all hover:border-slate-400 hover:bg-slate-50"
+         >
+            <Grid container gap={1} justifyContent="space-between" className="p-1.5 ">
+               <Grid item xs zeroMinWidth>
+                  <Typography fontWeight="bold" noWrap sx={{ textDecoration: "underline" }}>
+                     {establishment.name}
+                  </Typography>
+                  <Typography variant="caption" noWrap component="p">
+                     {establishment.industry}
+                  </Typography>
+               </Grid>
+               <Grid item xs="auto">
+                  <div className="flex flex-col gap-1">
+                     <Verified fontSize="small" />
+                     <Business fontSize="small" />
+                  </div>
+               </Grid>
+            </Grid>
+
+            <AspectContainedNextImage src={establishment.displayPhoto} alt={establishment.name} aspectRatio="3 / 2" />
+            <div className="p-2 pb-0">
+               <Typography component="p" variant="caption" color="grey" className="line-clamp-2">
+                  {establishment.about}
+               </Typography>
+            </div>
+         </Box>
+
+         <Box className="p-2 pt-0">
+            <div className="flex items-center justify-between gap-3 border-t-2">
+               <Typography noWrap className="flex-grow">
+                  <LocationOn fontSize="inherit" sx={{ mr: 0.2, mt: -0.2 }} />
+                  {establishment.location}
+               </Typography>
+
+               <SaveIncButton />
+            </div>
+         </Box>
+      </Box>
+   );
+}
---
+++ src/app/components/inc-connect/EventCard.tsx
@@ -0,0 +1,56 @@
+import { EventInterface } from "@/app/interfaces";
+import { Verified, LocationOn, Event } from "@mui/icons-material";
+import { Box, Grid, Typography } from "@mui/material";
+import Link from "next/link";
+import AspectContainedNextImage from "../general/AspectContainedNextImage";
+import SaveIncButton from "./SaveIncButton";
+
+export default function EventCard({ event, simplifified }: { event: EventInterface; simplifified?: boolean }) {
+   if (simplifified) {
+      console.log("simplified");
+   }
+
+   return (
+      <Box className="block border rounded-[15px] bg-white  ">
+         <Box
+            component={Link}
+            href={`/inc-connect/event/${event.slug}`}
+            className="block border pb-2 border-b-0 border-[transparent] rounded-tl-[15px] rounded-tr-[15px] cursor-pointer transition-all hover:border-slate-400 hover:bg-slate-50"
+         >
+            <Grid container gap={1} justifyContent="space-between" className="p-1.5 ">
+               <Grid item xs zeroMinWidth>
+                  <Typography fontWeight="bold" noWrap sx={{ textDecoration: "underline" }}>
+                     {event.name}
+                  </Typography>
+                  <Typography variant="caption" component="div">
+                     {event.eventType}
+                  </Typography>
+               </Grid>
+               <Grid item xs="auto">
+                  <div className="flex flex-col gap-1">
+                     <Verified fontSize="small" />
+                     <Event fontSize="small" />
+                  </div>
+               </Grid>
+            </Grid>
+
+            <AspectContainedNextImage src={event.displayPhoto} alt={event.name} aspectRatio="3 / 2" />
+            {/* <div className="p-2 pb-0">
+               <Typography component="p" variant="caption" color="grey" className="line-clamp-2">
+                  {event.about}
+               </Typography>
+            </div> */}
+         </Box>
+         <Box className="p-2 pt-0">
+            <div className="flex items-center justify-between gap-3 border-t-2">
+               <Typography noWrap className="flex-grow">
+                  <LocationOn fontSize="inherit" sx={{ mr: 0.2, mt: -0.2 }} />
+                  {event.location}
+               </Typography>
+
+               <SaveIncButton />
+            </div>
+         </Box>
+      </Box>
+   );
+}

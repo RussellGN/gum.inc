64b8ed00d08318c5abdf22c45bd3200f53c7b09d
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-03 13:15:02 +0200

    feat, ui: add back button to 404 page

--- src/app/(normal-pages)/[...catchAll]/page.tsx
+++ src/app/(normal-pages)/[...catchAll]/page.tsx
@@ -1,17 +1,18 @@
+import GoBackBtn from "@/app/components/general/GoBackBtn";
 import { Home } from "@mui/icons-material";
 import { Button, Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Page() {
    return (
-      <div className="min-h-[70vh] flex items-center justify-center">
-         <Typography textAlign="center">
-            This page is either still in development or does not exist
-            <br />
-            <Button startIcon={<Home />} component={Link} href="/" sx={{ mt: 2 }}>
+      <div className="min-h-[70vh] flex flex-col gap-3 items-center justify-center">
+         <Typography textAlign="center">This page is either still in development or does not exist</Typography>
+         <div className="flex items-center justify-center gap-3">
+            <GoBackBtn noMarginBottom />
+            <Button color="primary" startIcon={<Home />} component={Link} href="/">
                Homepage
             </Button>
-         </Typography>
+         </div>
       </div>
    );
 }

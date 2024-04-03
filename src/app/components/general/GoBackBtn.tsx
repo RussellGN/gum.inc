ee83913c1641c1926bafaa06537f21a25022db53
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-03 13:16:50 +0200

    feat: add option sx prop and make bottom margin optional

--- src/app/components/general/GoBackBtn.tsx
+++ src/app/components/general/GoBackBtn.tsx
@@ -1,20 +1,18 @@
 "use client";
 
 import { ArrowBack } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Button, SxProps } from "@mui/material";
 import { useRouter } from "next/navigation";
 
-export default function GoBackBtn() {
+export default function GoBackBtn({ sx, noMarginBottom }: { sx?: SxProps; noMarginBottom?: boolean }) {
    const router = useRouter();
 
    return (
       <Button
          onClick={() => router.back()}
-         // size="large"
-         // sx={{ border: "solid thin", borderColor: "divider", px: 3, mb: 3 }}
          color="primary"
          variant="outlined"
-         sx={{ mb: 3 }}
+         sx={{ ...sx, mb: noMarginBottom ? 0 : 3 }}
          startIcon={<ArrowBack />}
       >
          Go Back

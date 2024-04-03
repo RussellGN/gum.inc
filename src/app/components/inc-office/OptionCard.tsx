818724036e6e23b91e64065d7339c339c48624bd
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-03 13:18:40 +0200

    feat, refactor, ui: change appearance and convert to prop controlled component

--- src/app/components/inc-office/OptionCard.tsx
+++ src/app/components/inc-office/OptionCard.tsx
@@ -2,24 +2,29 @@
 
 import { option } from "@/app/types";
 import { Box, Checkbox, FormControlLabel } from "@mui/material";
-import { useState } from "react";
 
-export default function OptionCard({ option, index }: { option: option; index: number }) {
-   const [checked, setChecked] = useState(() => index === 0 || index === 1);
-   let color: string;
+type propTypes = {
+   option: option & { checked: boolean };
+   toggleOptionCheckedState: (optionName: string) => void;
+};
 
-   if (index % 3 === 0) color = "bg-stone-50";
-   else if (index % 2 === 0) color = "bg-zinc-100";
-   else color = "bg-zinc-200";
-
-   color = checked ? "bg-[rgba(255,255,255,0.5)] backdrop-blur" : "bg-white";
-   color = checked ? "bg-white" : "bg-[rgba(255,255,255,0.5)]";
-
+export default function OptionCard({ option, toggleOptionCheckedState }: propTypes) {
    return (
-      <Box className={`${color} p-2 border rounded-[5px] hover:bg-[rgba(255,255,255,0.8)]`}>
+      <Box
+         className="border shadow-md rounded-[15px] transition-all"
+         sx={{
+            px: 2,
+            py: 0.5,
+            color: option.checked ? "white" : "",
+            backgroundColor: option.checked ? "primary.light" : "white",
+            "&:hover": { backgroundColor: "primary.light", color: "white" },
+         }}
+      >
          <FormControlLabel
             name={option.name}
-            control={<Checkbox onChange={(e) => setChecked(e.target.checked)} checked={checked} />}
+            control={
+               <Checkbox color="default" onChange={() => toggleOptionCheckedState(option.name)} checked={option.checked} />
+            }
             label={option.name}
          />
       </Box>

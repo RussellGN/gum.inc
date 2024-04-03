7d558729598994fafb08ac032ec11a33292075f1
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-03 13:16:07 +0200

    refactor, ui: extract newsletter options selector into its own form component

--- src/app/(normal-pages)/inc-office/page.tsx
+++ src/app/(normal-pages)/inc-office/page.tsx
@@ -1,9 +1,7 @@
 import IncOfficeHero from "@/app/components/inc-office/IncOfficeHero";
-import OptionCard from "@/app/components/inc-office/OptionCard";
-import { researchBureauOptions } from "@/app/lib/constants";
-import { ArrowRightAlt, KeyboardDoubleArrowRight } from "@mui/icons-material";
-import { Box, Button, FormGroup, Grid, Typography } from "@mui/material";
-import Link from "next/link";
+import ResearchBureauNewsletterForm from "@/app/components/inc-office/ResearchBureauNewsletterForm";
+import { KeyboardDoubleArrowRight } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
 
 export default function Page() {
    return (
@@ -11,12 +9,10 @@ export default function Page() {
          <IncOfficeHero />
 
          <Box sx={{ px: 5, py: 8 }} className=" min-h-[60vh] my-5 ">
-            <Box
-               sx={{ backgroundColor: "secondary.main" }}
-               className="border rounded-[40px] w-full shadow-xl  text-center pt-10 pb-[12rem]"
-            >
+            <Box className="bg-white border-4 rounded-[30px] w-full shadow-xl pt-10 pb-[12rem]">
                <Typography
                   variant="h1"
+                  textAlign="center"
                   sx={{
                      width: "fit-content",
                      mx: "auto",
@@ -24,14 +20,13 @@ export default function Page() {
                      "&::after": {
                         content: '""',
                         position: "absolute",
-                        bottom: "-12px",
+                        bottom: "-15px",
                         height: "10px",
-                        width: "50%",
+                        width: "40%",
                         left: "50%",
                         borderRadius: "10px",
                         transform: "translateX(-50%)",
-                        background: "linear-gradient(to bottom right, white, whitesmoke)",
-                        // backgroundColor: "divider",
+                        backgroundColor: "divider",
                      },
                   }}
                >
@@ -39,56 +34,25 @@ export default function Page() {
                </Typography>
             </Box>
 
-            <div className="w-[95%] mx-auto relative -top-[8rem] ">
-               <Grid
-                  container
-                  gap={3}
-                  alignItems="center"
-                  justifyContent="center"
-                  className="p-10 rounded-[40px] "
-                  sx={{ background: "linear-gradient(to bottom, white, rgb(250, 250, 250))" }}
-               >
-                  <Grid item xs>
-                     <Box sx={{ px: 2, pt: 2, backgroundColor: "secondary.light", borderRadius: "10px" }}>
-                        <FormGroup sx={{ pb: 2 }} className="gap-2 max-h-[15rem] overflow-x-auto">
-                           {researchBureauOptions.map((option, index) => (
-                              <OptionCard key={index} index={index} option={option} />
-                           ))}
-                        </FormGroup>
-                     </Box>
-                  </Grid>
+            <Box className="bg-[whitesmoke] border-4 shadow-xl w-[96%] mx-auto relative -top-[8rem] p-12 rounded-[30px]">
+               <Typography sx={{ mb: 2 }}>
+                  Dive into the realm of business innovation with our Research Bureau Newsletter. Choose from a variety of
+                  focus areas and receive curated insights and valuable updates to stay informed and stay ahead
+               </Typography>
 
-                  <Grid item xs={12} md={5}>
-                     <div className="max-w-prose">
-                        <Typography sx={{ mb: 1 }}>
-                           Dive into the realm of business innovation with our Research Bureau Newsletter. Choose from a
-                           variety of focus areas and receive curated insights and valuable updates to stay informed and
-                           stay ahead
-                        </Typography>
-                        <Typography fontWeight="bold" className="flex items-center gap-2">
-                           <KeyboardDoubleArrowRight fontSize="inherit" />
-                           Select 3-30 to proceed (each at a price of £10)
-                        </Typography>
-                        <Typography fontWeight="bold" className="flex items-center gap-2">
-                           <KeyboardDoubleArrowRight fontSize="inherit" />
-                           Due Dillignce (All) for £30{" "}
-                        </Typography>
-                        <div className="flex items-center gap-3 mt-4 ">
-                           <Button
-                              component={Link}
-                              href="/subscribe"
-                              size="large"
-                              color="primary"
-                              endIcon={<ArrowRightAlt />}
-                           >
-                              Proceed To Subscription
-                           </Button>
-                           <Button>Read More</Button>
-                        </div>
-                     </div>
-                  </Grid>
-               </Grid>
-            </div>
+               <Box sx={{ mb: 4 }} className="flex gap-3 items-center">
+                  <Typography fontWeight="bold" className="flex items-center gap-2">
+                     <KeyboardDoubleArrowRight fontSize="inherit" />
+                     Select 3-30 to proceed (each at a price of £10)
+                  </Typography>
+                  <Typography fontWeight="bold" className="flex items-center gap-2">
+                     <KeyboardDoubleArrowRight fontSize="inherit" />
+                     Due Dillignce (All) for £30{" "}
+                  </Typography>
+               </Box>
+
+               <ResearchBureauNewsletterForm />
+            </Box>
          </Box>
       </>
    );
---
+++ src/app/components/inc-office/ResearchBureauNewsletterForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { researchBureauOptions } from "@/app/lib/constants";
+import { ArrowRightAlt, DoneAll, RemoveDone } from "@mui/icons-material";
+import { Button, FormGroup, Box } from "@mui/material";
+import Link from "next/link";
+import { useState } from "react";
+import OptionCard from "./OptionCard";
+
+export default function ResearchBureauNewsletterForm() {
+   const [options, setOptions] = useState(() => researchBureauOptions.map((option) => ({ ...option, checked: false })));
+
+   const numberOfoptionsSelected = options.filter((option) => option.checked).length;
+   const isAllSelected = options.length === numberOfoptionsSelected;
+
+   function toggleOptionCheckedState(optionName: string) {
+      let newOptions = [...options];
+      newOptions.forEach((option, index) => {
+         if (option.name === optionName) {
+            newOptions[index].checked = !option.checked;
+         }
+      });
+      setOptions(newOptions);
+   }
+
+   function toggleAllOptions() {
+      let newOptions = [...options];
+      if (isAllSelected) {
+         newOptions.forEach((option) => {
+            option.checked = false;
+         });
+      } else {
+         newOptions.forEach((option) => {
+            option.checked = true;
+         });
+      }
+      setOptions(newOptions);
+   }
+
+   return (
+      <form>
+         <div className="flex gap-3">
+            <Button
+               disabled={numberOfoptionsSelected < 3}
+               type="submit"
+               color="success"
+               component={Link}
+               href="/subscribe"
+               size="medium"
+               endIcon={<ArrowRightAlt />}
+            >
+               Proceed
+            </Button>
+            <Button
+               onClick={toggleAllOptions}
+               color="success"
+               size="medium"
+               variant="outlined"
+               endIcon={isAllSelected ? <RemoveDone /> : <DoneAll />}
+            >
+               {isAllSelected ? "Unselect all" : "Select all"}
+            </Button>
+         </div>
+
+         <Box sx={{ mt: 2.5 }}>
+            <FormGroup sx={{ pb: 2 }} className="">
+               <div className="flex flex-row flex-wrap gap-3">
+                  {options.map((option, index) => (
+                     <OptionCard
+                        key={option.name}
+                        index={index}
+                        option={option}
+                        toggleOptionCheckedState={toggleOptionCheckedState}
+                     />
+                  ))}
+               </div>
+            </FormGroup>
+         </Box>
+      </form>
+   );
+}

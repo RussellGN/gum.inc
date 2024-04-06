be6b8e1d9378e43bfd092c9a55c6a219d4189f16
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-06 19:48:03 +0200

    feat, ui: create links to event and establishment forms

--- src/app/(normal-pages)/dashboard/layout.tsx
+++ src/app/(normal-pages)/dashboard/layout.tsx
@@ -1,14 +1,41 @@
 import DashboardNav from "@/app/components/dashboard/DashboardNav";
-import { Grid } from "@mui/material";
+import { Business, EventAvailable } from "@mui/icons-material";
+import { Button, Grid } from "@mui/material";
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 export default function layout({ children }: PropsWithChildren) {
    return (
-      <Grid container gap={2} wrap="nowrap">
-         <DashboardNav />
-         <Grid item xs={11}>
-            <div>{children}</div>
+      <>
+         <div className="flex gap-3 items-center justify-end mb-4 ">
+            <Button
+               size="medium"
+               variant="outlined"
+               color="primary"
+               component={Link}
+               href="/new/event"
+               endIcon={<EventAvailable />}
+            >
+               Create Event Page
+            </Button>
+            <Button
+               size="medium"
+               variant="outlined"
+               color="primary"
+               component={Link}
+               href="/new/establishment"
+               endIcon={<Business />}
+            >
+               Create Business / Organisation Page
+            </Button>
+         </div>
+
+         <Grid container gap={2} wrap="nowrap">
+            <DashboardNav />
+            <Grid item xs={11}>
+               <div>{children}</div>
+            </Grid>
          </Grid>
-      </Grid>
+      </>
    );
 }

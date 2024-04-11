acbb7ee1cbd4eb6bac391c5a609bc68f3ae1a93b
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-11 12:25:03 +0200

    bump: implement optional small prop

--- src/app/components/inc-office/OptionCard.tsx
+++ src/app/components/inc-office/OptionCard.tsx
@@ -1,25 +1,32 @@
 "use client";
 
 import { option } from "@/app/types";
-import { Box, Checkbox, FormControlLabel } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, SxProps } from "@mui/material";
 
 type propTypes = {
    option: option & { checked: boolean };
    toggleOptionCheckedState: (optionName: string) => void;
+   small?: boolean;
 };
 
-export default function OptionCard({ option, toggleOptionCheckedState }: propTypes) {
+export default function OptionCard({ option, toggleOptionCheckedState, small }: propTypes) {
+   const sx: SxProps = small
+      ? {
+           px: 1,
+           py: 0,
+           color: option.checked ? "white" : "",
+           backgroundColor: option.checked ? "primary.light" : "white",
+           "&:hover": { backgroundColor: "primary.light", color: "white" },
+        }
+      : {
+           px: 2,
+           py: 0.5,
+           color: option.checked ? "white" : "",
+           backgroundColor: option.checked ? "primary.light" : "white",
+           "&:hover": { backgroundColor: "primary.light", color: "white" },
+        };
    return (
-      <Box
-         className="border shadow-md rounded-[15px] transition-all"
-         sx={{
-            px: 2,
-            py: 0.5,
-            color: option.checked ? "white" : "",
-            backgroundColor: option.checked ? "primary.light" : "white",
-            "&:hover": { backgroundColor: "primary.light", color: "white" },
-         }}
-      >
+      <Box className="border shadow-sm rounded-[15px] transition-all" sx={sx}>
          <FormControlLabel
             name={option.name}
             control={

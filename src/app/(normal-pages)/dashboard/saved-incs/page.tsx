45e9348ba3960b222a9cac79d2e819be632f6e9e
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-11 12:32:55 +0200

    refactor, ui: refactor saved incs

--- src/app/(normal-pages)/dashboard/saved-incs/page.tsx
+++ src/app/(normal-pages)/dashboard/saved-incs/page.tsx
@@ -1,18 +1,51 @@
+import EventCard from "@/app/components/inc-connect/EventCard";
+import OrganizationCard from "@/app/components/inc-connect/OrganizationCard";
 import SearchAndControls from "@/app/components/inc-connect/SearchAndControls";
 import UserCard from "@/app/components/inc-connect/UserCard";
-import { sampleUsers } from "@/app/lib/sampleData";
-import { Box, Pagination } from "@mui/material";
+import { OrganizationInterface, UserInterface } from "@/app/interfaces";
+import { sampleEvents, sampleOrganizationsForFeature, sampleUsersForFeature } from "@/app/lib/sampleData";
+import { Box, Typography } from "@mui/material";
 
 export default function page() {
+   const peopleEntities = [...sampleUsersForFeature, ...sampleOrganizationsForFeature]
+      .sort((listing1, listing2) => listing1.name.localeCompare(listing2.name))
+      .splice(0, 10);
+
+   const events = sampleEvents.splice(0, 5);
+
    return (
       <Box sx={{ px: 5 }}>
          <SearchAndControls />
-         <div className="grid grid-cols-5 gap-2 ">
-            {sampleUsers.map((user) => (
-               <UserCard key={user.slug} user={user} />
-            ))}
-         </div>
-         <Pagination page={1} count={5} className="w-fit mx-auto my-[5rem]" />
+
+         <Box sx={{ mb: 5 }}>
+            <Typography variant="h3" className="w-fit border-b-8 pb-2" sx={{ mb: 4 }}>
+               Events
+            </Typography>
+            <div className="grid grid-cols-5 gap-2 ">
+               {events.map((event) => (
+                  <EventCard initiallySaved key={event.slug} event={event} />
+               ))}
+            </div>
+         </Box>
+
+         <Box sx={{ mb: 10 }}>
+            <Typography variant="h3" className="w-fit border-b-8 pb-2" sx={{ mb: 4 }}>
+               People, Businesses & Other...
+            </Typography>
+            <div className="grid grid-cols-5 gap-2 ">
+               {peopleEntities.map((peopleEntity) =>
+                  peopleEntity.type === "user" ? (
+                     <UserCard initiallySaved key={peopleEntity.slug} user={peopleEntity as UserInterface} />
+                  ) : (
+                     <OrganizationCard
+                        initiallySaved
+                        key={peopleEntity.slug}
+                        organization={peopleEntity as OrganizationInterface}
+                     />
+                  )
+               )}
+            </div>
+         </Box>
       </Box>
    );
 }

1e2a58f2b438c6c5bc6bd4dc257819ed44e845af
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-11 12:26:52 +0200

    refactor: refactor sidebar

--- src/app/components/inc-connect/Sidebar.tsx
+++ src/app/components/inc-connect/Sidebar.tsx
@@ -1,16 +1,11 @@
-"use client";
-
-import { Box, Grid, TextField, Typography } from "@mui/material";
-import { Suspense, useState } from "react";
-import { expandSize } from "@/app/types";
+import { Box, Grid, Typography } from "@mui/material";
+import { Suspense } from "react";
 import Image from "next/image";
 import DirsAccordion from "./DirsAccordion";
 
 export default function Sidebar() {
-   const [expand] = useState<expandSize>("normal");
-
    return (
-      <Grid item xs md={expand === "normal" ? 2.5 : "auto"}>
+      <Grid item xs md={2.5}>
          <Box className="h-[80vh] overflow-y-auto border rounded-[15px] bg-white p-4 sticky top-[100px]">
             <Image
                src="/svgs/undraw_location_search_re_ttoj.svg"
@@ -20,30 +15,13 @@ export default function Sidebar() {
                className="w-full mb-4 mx-auto max-w-[7rem]"
             />
 
-            <Typography variant="h5" textAlign="center">
+            <Typography variant="h5" textAlign="center" sx={{ mb: 3 }}>
                Almanacs & Directories
             </Typography>
-
-            <div className="flex justify-center gap-2 items-center mt-3 mb-5">
-               <TextField label="Search..." placeholder="e.g Finance" size="small" />
-            </div>
 
             <Suspense>
                <DirsAccordion />
             </Suspense>
-
-            {/* <ul className="list-none">
-               {directories.map((dir) => (
-                  <li key={dir.name} onClick={() => viewDirectory(dir.name)} className="flex gap-1 items-center">
-                     <div className="flex-grow">
-                        <DirectoryCard dir={dir} activeDir={activeDir} expand={expand} />
-                     </div>
-                     <IconButton size="small">
-                        <KeyboardArrowDown />
-                     </IconButton>
-                  </li>
-               ))}
-            </ul> */}
          </Box>
       </Grid>
    );

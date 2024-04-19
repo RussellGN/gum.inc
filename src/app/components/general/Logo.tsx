af4539022b4c5b049a42f0f0101ed061a31541b3
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-19 12:58:19 +0200

    refactor, ui: revamp logo

--- src/app/components/general/Logo.tsx
+++ src/app/components/general/Logo.tsx
@@ -1,9 +1,23 @@
+import { gradients } from "@/app/lib/constants";
 import { Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Logo() {
    return (
-      <Typography component={Link} href="/" color="primary" variant="h3">
+      <Typography
+         component={Link}
+         href="/"
+         color="primary"
+         variant="h3"
+         sx={{
+            position: "relative",
+            display: "inline-block",
+            background: gradients.primary1,
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "bolder",
+         }}
+      >
          GUM.INC
       </Typography>
    );

6e2b835c62a790aabeea6403c8f76805ee925eba
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-19 12:54:46 +0200

    feat, ui: create contact page

---
+++ src/app/(form-pages)/contact/page.tsx
@@ -0,0 +1,87 @@
+import DividerWithText from "@/app/components/general/DividerWithText";
+import { Link as MuiLink, TextField, Typography, Button } from "@mui/material";
+
+export default function Page() {
+   return (
+      <form>
+         <Typography sx={{ mb: 3 }} variant="h2" textAlign="center">
+            Get In Touch
+         </Typography>
+
+         <MuiLink
+            fontWeight="bold"
+            sx={{ display: "block", textAlign: "center", mb: 1 }}
+            component="a"
+            href="mailto:info@guminc.com"
+         >
+            Email - info@guminc.com
+         </MuiLink>
+
+         <DividerWithText text="or" />
+
+         <div className="flex flex-col gap-5 p-5">
+            <div className=" flex items-center gap-3">
+               <div className="flex-grow">
+                  <TextField
+                     variant="filled"
+                     size="small"
+                     label="Your Email"
+                     name="email"
+                     type="email"
+                     placeholder="example@example.com"
+                     fullWidth
+                     required
+                     inputProps={{ min: 3, max: 50 }}
+                     sx={{ "& .MuiInputBase-root": { borderRadius: "10px 10px 0 0" } }}
+                  />
+               </div>
+               <div className="flex-grow">
+                  <TextField
+                     variant="filled"
+                     size="small"
+                     label="Name"
+                     name="name"
+                     placeholder="Your name"
+                     fullWidth
+                     required
+                     inputProps={{ min: 3, max: 50 }}
+                     sx={{ "& .MuiInputBase-root": { borderRadius: "10px 10px 0 0" } }}
+                  />
+               </div>
+            </div>
+
+            <TextField
+               variant="filled"
+               size="small"
+               label="Subject"
+               name="subject"
+               placeholder="What is the nature of your email?"
+               fullWidth
+               required
+               inputProps={{ min: 3, max: 50 }}
+               sx={{ "& .MuiInputBase-root": { borderRadius: "10px 10px 0 0" } }}
+            />
+            <TextField
+               variant="filled"
+               size="small"
+               label="Your Message"
+               name="message"
+               multiline
+               rows={3}
+               placeholder="Type your message..."
+               fullWidth
+               required
+               inputProps={{ min: 2, max: 500 }}
+               sx={{ "& .MuiInputBase-root": { borderRadius: "10px 10px 0 0" } }}
+            />
+         </div>
+
+         <div className="text-right px-8">
+            {/* <Button color="primary" type="submit"> */}
+            <Button color="primary" type="button">
+               Send
+            </Button>
+         </div>
+      </form>
+   );
+}

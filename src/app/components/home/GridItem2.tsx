b238cc1ee94c9c1250e159501a1a5d2b595685fd
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-21 23:11:20 +0200

    fix, data: move sample data item into component instead of searching through an array

--- src/app/components/home/GridItem2.tsx
+++ src/app/components/home/GridItem2.tsx
@@ -1,12 +1,121 @@
-import { sampleUsers } from "@/app/lib/sampleData";
 import { Bookmarks, CheckCircleOutlined } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import Image from "next/image";
 import UncontrolledSlideshow from "../general/UncontrolledSlideshow";
+import images from "@/app/assets";
+
+const users = [
+   {
+      type: "user",
+      worksAt: "Independant",
+      id: 6,
+      slug: "tom-lloyd-6",
+      name: "Tom Lloyd",
+      email: "lloydtm@gmail.com",
+      jobTitle: "Organic farm specialist",
+      industry: "Farming",
+      location: "Manchester",
+      verified: true,
+      bio: "Dedicated farmer with a passion for sustainable agriculture and land stewardship. Specializes in cultivating organic crops and raising livestock using environmentally friendly practices. Experienced in crop rotation, soil health management, and integrated pest management to ensure high yields and quality produce. Committed to promoting local food systems and community engagement in agriculture.",
+      displayPhoto: images.user2m,
+      images: [images.farm_logo, images.farm, images.farm1, images.garden],
+      serviceOfferings: [
+         {
+            service: "Organic Farming",
+            description:
+               "Cultivating a variety of organic crops using methods that prioritize soil health, biodiversity, and environmental sustainability.",
+         },
+         {
+            service: "Livestock Management",
+            description:
+               "Raising healthy and happy animals through ethical and humane practices, focusing on animal welfare and quality of products.",
+         },
+         {
+            service: "Farm-to-Table Sales",
+            description:
+               "Selling fresh produce and farm products directly to consumers through farmer's markets, community-supported agriculture (CSA), and online platforms.",
+         },
+         {
+            service: "Agricultural Consulting",
+            description:
+               " Providing expert advice and guidance on sustainable farming practices, crop selection, and farm management techniques to aspiring farmers and agricultural businesses.",
+         },
+      ],
+      socialMedia: [
+         {
+            platform: "LinkedIn",
+            username: "farmerLloyd",
+            url: "#",
+         },
+         {
+            platform: "Twitter",
+            username: "farmerLloyd",
+            url: "#",
+         },
+      ],
+   },
+   {
+      type: "user",
+      worksAt: "Independant",
+      id: 7,
+      slug: "steven-tyler-7",
+      name: "Steven Tyler",
+      email: "tyler@yahoo.co.uk",
+      jobTitle: "Dj",
+      industry: "Music",
+      location: "London",
+      verified: true,
+      bio: "Dynamic bass DJ renowned for electrifying performances and infectious beats that keep crowds dancing all night. Specializes in mixing and blending bass-heavy tracks across various genres, from dubstep to trap to drum and bass. Known for creating immersive sonic experiences and fostering a vibrant atmosphere at clubs, festivals, and events worldwide.",
+      displayPhoto: images.person9m,
+      images: [images.event2, images.event3, images.event7],
+      serviceOfferings: [
+         {
+            service: "Live Performances",
+            description:
+               "Delivering high-energy DJ sets featuring bass-heavy music, customized to suit the vibe and audience preferences of each event.",
+         },
+         {
+            service: "Event Booking",
+            description:
+               "Available for bookings at clubs, festivals, concerts, private parties, and corporate events, bringing unparalleled energy and excitement to any occasion.",
+         },
+         {
+            service: "Music Production",
+            description:
+               "Producing original bass music tracks and remixes for release on digital platforms, showcasing unique soundscapes and innovative production techniques.",
+         },
+         {
+            service: "DJ Workshops",
+            description:
+               "Offering workshops and tutorials on bass DJing techniques, mixing fundamentals, and music production tips to aspiring DJs and electronic music enthusiasts.",
+         },
+      ],
+      socialMedia: [
+         {
+            platform: "LinkedIn",
+            username: "djTyler",
+            url: "#",
+         },
+         {
+            platform: "Facebook",
+            username: "djTyler",
+            url: "#",
+         },
+         {
+            platform: "Instagram",
+            username: "djTyler",
+            url: "#",
+         },
+         {
+            platform: "Twitter",
+            username: "djTyler",
+            url: "#",
+         },
+      ],
+   },
+];
 
 export default function GridItem2() {
-   const users = sampleUsers.splice(7, 2);
-
    const slides = [
       <div key="item2-slide1" className="h-full w-full flex justify-around gap-3 p-2">
          {users.map((user) => (

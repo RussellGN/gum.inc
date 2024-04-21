7bc24069bef45638a29930345aeb99bf716c05a5
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-21 23:12:10 +0200

    feat, refactor, ui: extract graphic into own component and  implement mobile responsiveness

--- src/app/components/home/ValueProposition.tsx
+++ src/app/components/home/ValueProposition.tsx
@@ -1,61 +1,19 @@
-import images from "@/app/assets";
-import { ArrowRightAlt, Message, Search } from "@mui/icons-material";
+import { ArrowRightAlt } from "@mui/icons-material";
 import { Box, Button, Grid, Typography } from "@mui/material";
-import Image from "next/image";
 import Link from "next/link";
+import ValuePropositionGraphic from "./ValuePropositionGraphic";
 
 export default function ValueProposition() {
    return (
       <Grid container alignItems="center" justifyContent="center" className="min-h-[90vh]">
-         <Grid item xs md={5}>
-            <Box className="h-full  p-5 relative flex items-center justify-center">
-               <Box className="flex py-1 justify-center h-[28rem] bg-gray-200 border-gray-200 border-4 rounded-[20px]  shadow-md relative">
-                  <Image
-                     src={images.inc_user_page_screenshot}
-                     alt="screenshot of a user's page on the platform"
-                     className="w-auto h-full rounded-[20px]"
-                  />
-                  <Box
-                     sx={{ backgroundColor: "primary.light" }}
-                     className="border h-[25rem] w-[15rem]  rounded-lg shadow-md absolute top-[-3rem] -right-1/4 rotate-6 -z-10"
-                  ></Box>
-
-                  <Box className="bg-white border rounded-lg shadow-lg absolute top-1/4 -left-full">
-                     <div className="py-1 px-2 border gap-4 flex items-center justify-around">
-                        <div className="flex items-center gap-2">
-                           <Search fontSize="small" />
-                           <Typography variant="subtitle1">Tax Lawyer, London</Typography>
-                        </div>
-                        <Typography variant="caption" className="bg-gray-200 border rounded-lg px-2">
-                           found 32
-                        </Typography>
-                     </div>
-                  </Box>
-
-                  <Box className="bg-white border rounded-lg shadow-lg absolute bottom-[3rem] -right-3/4">
-                     <div className="flex items-center gap-4 py-0.5 px-1.5">
-                        <Image
-                           src={images.person2m}
-                           alt="user"
-                           className="w-10 h-10 object-cover rounded-[100%] border-2 shadow-md"
-                        />
-                        <div className="mr-auto flex flex-col gap-0">
-                           <Typography sx={{ mb: 0 }} variant="subtitle2">
-                              Aaron Rhodes
-                           </Typography>
-                           <Typography variant="caption">Tax Lawyer</Typography>
-                        </div>
-                        <Message fontSize="small" />
-                     </div>
-                  </Box>
-               </Box>
-            </Box>
+         <Grid item xs={8} md={5}>
+            <ValuePropositionGraphic />
          </Grid>
 
-         <Grid item xs md={5} sx={{ pl: { md: 8 } }}>
-            <Box sx={{ pr: { md: 5 } }}>
+         <Grid item xs={12} md={5} sx={{ pl: { md: 8 } }}>
+            <Box sx={{ textAlign: { xs: "center", md: "left" }, pr: { md: 5 } }}>
                <Typography variant="h2">Yellow pages done right</Typography>
-               <Typography className="max-w-prose py-3">
+               <Typography className="max-w-prose py-3" sx={{ px: { xs: 1, md: "unset" } }}>
                   GUM.INC brings together businesses, proffessionals, creatives and other entities onto one platform for
                   easy discovery and connecting
                </Typography>

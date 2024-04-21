f37428e599b9a19b5c2547724d0be962ef1ebf07
Russellg <103833866+RussellGN@users.noreply.github.com>
2024-04-21 22:46:27 +0200

    refactor, feat: change friendlyDate to return various formats, Create util function that determines elapsed time

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -67,7 +67,45 @@ export function capitalizeWords(str: string) {
    return finalString;
 }
 
-export function friendlyDate(date: string | Date) {
+export function friendlyDate(date: string | Date, format?: "second" | "third") {
    date = new Date(date);
-   return Intl.DateTimeFormat("en-us", { month: "short", day: "2-digit", year: "numeric" }).format(date);
+   switch (format) {
+      case "second":
+         return Intl.DateTimeFormat("en-GB", { month: "short", day: "2-digit", year: "numeric" }).format(date);
+      case "third":
+         return Intl.DateTimeFormat("en-GB", { month: "numeric", day: "2-digit", year: "2-digit" }).format(date);
+      default:
+         return Intl.DateTimeFormat("en-GB", { month: "short", day: "2-digit", year: "2-digit" }).format(date);
+   }
+}
+
+export function getTimeElapsedSince(date: Date | string): string {
+   const currentDate = new Date();
+   const startDate = typeof date === "string" ? new Date(date) : date;
+
+   const elapsedMilliseconds = currentDate.getTime() - startDate.getTime();
+
+   if (elapsedMilliseconds < 1) return "now";
+
+   const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+   const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+   const elapsedHours = Math.floor(elapsedMinutes / 60);
+   const elapsedDays = Math.floor(elapsedHours / 24);
+   const elapsedWeeks = Math.floor(elapsedDays / 7);
+   const elapsedMonths = Math.floor(elapsedDays / 30);
+   const elapsedYears = Math.floor(elapsedDays / 365);
+
+   if (elapsedYears > 0) {
+      return `${elapsedYears} y`;
+   } else if (elapsedMonths > 0) {
+      return `${elapsedMonths} m`;
+   } else if (elapsedWeeks > 0) {
+      return `${elapsedWeeks} w`;
+   } else if (elapsedDays > 0) {
+      return `${elapsedDays} d`;
+   } else if (elapsedHours > 0) {
+      return `${elapsedHours} hr`;
+   } else {
+      return `${elapsedMinutes} min`;
+   }
 }
